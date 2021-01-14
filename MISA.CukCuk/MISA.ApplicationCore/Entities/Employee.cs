627bd80991600d157ce66085711726e04dc88d71
vutuanminh34 <vutuanminh34@gmail.com>
2021-01-14 17:30:35 +0700

    new changing

--- MISA.CukCuk/MISA.ApplicationCore/Entities/Employee.cs
+++ MISA.CukCuk/MISA.ApplicationCore/Entities/Employee.cs
@@ -7,8 +7,21 @@ namespace MISA.ApplicationCore.Entities
     public class Employee : BaseEntity
     {
         #region Property
+        /// <summary>
+        /// Khóa chính
+        /// </summary>
+        [PrimaryKey]
         public Guid EmployeeId { get; set; }
+
+        /// <summary>
+        /// Mã nhân viên
+        /// </summary>
+        [Required]
+        [DisplayName("mã nhân viên")]
+        [MaxLength(20, "Mã nhân viên không vượt quá 20 ký tự!")]
         public string EmployeeCode { get; set; }
+
+        [DisplayName("họ và tên")]
         public string FullName { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public int? Gender { get; set; }
@@ -16,6 +29,12 @@ namespace MISA.ApplicationCore.Entities
         public DateTime? IdentificationDate { get; set; }
         public string IdentificationLocation { get; set; }
         public string Email { get; set; }
+
+        /// <summary>
+        /// Số điện thoại
+        /// </summary>
+        [CheckDuplicate]
+        [DisplayName("số điện thoại")]
         public string PhoneNumber { get; set; }
         public Guid? PositionId { get; set; }
         public string PositionName { get; set; }
